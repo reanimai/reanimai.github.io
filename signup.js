6efac9d041e4fcc7f948274e629cfe8b9c592c11
reanimai <antonis@reanimai.com>
2020-04-05 22:36:56 +0100

    loading while signing up

--- signup.js
+++ signup.js
@@ -21,7 +21,7 @@ function signup(e) {
     var form = document.getElementById("contact-form");
     var status = document.getElementById("contact-form-status");
     form.style.display = "none";
-    status.innerHTML = "We are signing you up...";
+    var status_interval = setInterval(loading_status, 300)
 
    $.ajax({
      type: "POST",
@@ -36,8 +36,43 @@ function signup(e) {
      },
      error: function () {
         status.innerHTML = "There was a problem submitting your email.";
+     },
+     complete: function(){
+        clearInterval(status_interval)
      }});
 }
+
+
+function loading_status(){
+    var status = document.getElementById("contact-form-status");
+
+    base_str = "We are signing you up"
+    if (status.innerHTML == null || status.innerHTML == '') {
+      status.innerHTML = base_str + "   ";
+    }
+
+    dots = 1
+    for (var i = 0; i < 3; i++) {
+      if (status.innerHTML[i + base_str.length] != '.') {
+        break
+      }
+      dots += 1
+    }
+    if (dots == 4){
+      dots = 1
+    }
+    
+    status.innerHTML = base_str;
+    for (var i = 1; i < 4; i++) {
+      if (i <= dots){
+        status.innerHTML = status.innerHTML + '.'
+      }
+      else{
+        status.innerHTML = status.innerHTML + '&nbsp;'
+      }
+    }
+}
+
 
 function stoppedTyping(text){
       if(text.value.length > 0) { 
